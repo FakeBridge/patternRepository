e193d2a7fd9ea72582b440fbae5af2d8cfaab27a
Senkovičová Soňa <36665069+senkovicova1@users.noreply.github.com>
2021-03-30 22:12:19 +0200

    Update firebase.tsx

--- patternrepository/src/firebase.tsx
+++ patternrepository/src/firebase.tsx
@@ -2,15 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: 'AIzaSyBSJOrJyHwTWwFW06_SQkOcNOExcFUuVdE',
-    authDomain: 'patternrepository-e8c44.firebaseapp.com',
-    projectId: 'patternrepository-e8c44',
-    storageBucket: 'patternrepository-e8c44.appspot.com',
-    messagingSenderId: '658185200448',
-    appId: '1:658185200448:web:9f55f3f53845c2d94cd0a5',
-    measurementId: 'G-V4Z0TXZV4C',
-};
+
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
